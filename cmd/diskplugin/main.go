492469f3a0952bb10744654fa83a1a18fee638dc
tianyichen <tianyichen@kingsoft.com>
2024-04-23 19:25:22 +0800

    modify default mountPermissions

--- cmd/diskplugin/main.go
+++ cmd/diskplugin/main.go
@@ -54,7 +54,7 @@ var (
 	driverName        = flag.String("driver", DiskNFSMultiDriverName, "CSI Driver, support multi driver and  separated by ','")
 	maxVolumesPerNode = flag.Int64("max-volumes-pernode", 8, "Only EBS: maximum number of volumes that can be attached to node")
 	//nfs
-	mountPermissions      = flag.Uint64("mount-permissions", 0777, "mounted folder permissions")
+	mountPermissions      = flag.Uint64("mount-permissions", 0, "mounted folder permissions")
 	workingMountDir       = flag.String("working-mount-dir", "/tmp", "working directory for provisioner to mount nfs shares temporarily")
 	defaultOnDeletePolicy = flag.String("default-ondelete-policy", "", "default policy for deleting subdirectory when deleting a volume")
 )
